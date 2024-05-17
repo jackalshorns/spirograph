e1b2008ab3eac2abb8da350f000548c8284b9092
jackalshorns <jason@uxisfine.com>
2024-05-17 15:35:50 -0500

    Final Spirograph Project

--- mail.py
+++ mail.py
@@ -3,34 +3,23 @@ import random
 
 tim = Turtle()
 screen = Screen()
-# Old Shizzy w/ Stamps
-# # RADIUS = 1
-# # CURSOR_RADIUS = 1
-#
-# tim.hideturtle()
-# tim.shape('circle')
-# #tim.shapesize(RADIUS / CURSOR_RADIUS, outline=RADIUS/5)
-# tim.turtlesize(stretch_len=50, stretch_wid=50)
-# tim.penup()
-# #tim.circle(RADIUS)
-# # tim.stamp()
-# # tim.forward(100)
-# # tim.stamp()
-# # """
-#
-# for i in range(36):
-#     steps = 10
-#     angle = 10
-#     tim.right(angle)
-#     tim.fd(steps)
-#     r = random.randint(0, 1)
-#     g = random.randint(0, 1)
-#     b = random.randint(0, 1)
-#     tim.color(r, g, b)
-#     tim.fillcolor("")
-#     tim.stamp()
 
-tim.circle(100)
+TILT = 20
+RADIUS = 100
+
+tim.speed("fastest")
+
+def draw_circle():
+    for i in range(int(360/TILT) - 1):
+        r = random.random()
+        g = random.random()
+        b = random.random()
+        tim.color(r, g, b)
+        tim.circle(RADIUS)
+        tim.left(TILT)
+
+draw_circle()
+
 
 # Exit on click
 screen.exitonclick()
